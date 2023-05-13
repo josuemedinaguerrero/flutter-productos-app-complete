936fcac826fd9fda828e11bb1ccd54ded51fa2f1
Josué Medina Guerrero <josuemedina.032810@gmail.com>
2023-05-13 02:20:57 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:productos_app/screens/screens.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      initialRoute: 'login',
+      routes: {
+        'login': (_) => const LoginScreen(),
+        'home': (_) => const HomeScreen(),
+      },
+      theme: ThemeData.light().copyWith(scaffoldBackgroundColor: Colors.grey[300]),
+    );
+  }
+}
